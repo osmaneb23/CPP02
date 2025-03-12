503b264474f137b0c17645235ab60dde2247e4ae
Osmane <osmane.sgb@gmail.com>
2025-03-12 02:38:07 +0100

    ex01 created

---
+++ ex01/includes/Fixed.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: obouayed <obouayed@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/11 22:15:52 by obouayed          #+#    #+#             */
+/*   Updated: 2025/03/12 02:27:24 by obouayed         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <cmath>
+# include <iostream>
+
+class Fixed
+{
+  private:
+	int _raw;
+	static const int _nbits = 8;
+
+  public:
+	Fixed(void);
+	Fixed(const int n);
+	Fixed(const float f);
+	Fixed(const Fixed &obj);
+	Fixed &operator=(const Fixed &obj);
+	~Fixed(void);
+
+	int getRawBits(void) const;
+	void setRawBits(int const raw);
+	float toFloat(void) const;
+	int toInt(void) const;
+};
+
+#endif
